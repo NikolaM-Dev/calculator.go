73714efb2cf581fbd8539ef403e604dcf245ba0e
Juan David Merchan Torres <juan1014166@gmail.com>
2024-02-03 11:41:15 -0500

    feat(calculator): Add Divide func

--- calculator.go
+++ calculator.go
@@ -1,6 +1,10 @@
 // Package calculator does simple calculations
 package calculator
 
+import (
+	"errors"
+)
+
 // Add takes numbers and
 // returns the result of adding them together
 func Add(a, b float64) float64 {
@@ -17,4 +21,14 @@ func Subtract(a, b float64) float64 {
 // returns the result of multiplying them together
 func Multiply(a, b float64) float64 {
 	return a * b
+}
+
+// Divide takes two numbers a and b, and
+// returns the result of dividing a by b
+func Divide(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, errors.New("division by zero not allowed")
+	}
+
+	return a / b, nil
 }
