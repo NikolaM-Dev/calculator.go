aa5044ae8a4bb1c04405ac255f4ac892237f40ba
Juan David Merchan Torres <juan1014166@gmail.com>
2024-02-03 08:11:27 -0500

    feat(calculator): Add base calculator package

---
+++ calculator.go
@@ -0,0 +1,7 @@
+// Package calculator does simple calculations
+package calculator
+
+// Add takes numbers an returns the result of adding them together
+func Add(a, b float64) float64 {
+	return a + b
+}
