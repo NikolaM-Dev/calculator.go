27108f944bf6847afaa619b49ae892a3a236ab3e
Juan David Merchan Torres <juan1014166@gmail.com>
2024-02-03 09:44:48 -0500

    test(calculator_test): Change to use tests cases

--- calculator_test.go
+++ calculator_test.go
@@ -9,32 +9,71 @@ import (
 func TestAdd(t *testing.T) {
 	t.Parallel()
 
-	var want float64 = 4
-	got := calculator.Add(2, 2)
+	type testCase struct {
+		a, b float64
+		want float64
+	}
 
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+	testCases := []testCase{
+		{a: 2, b: 2, want: 4},
+		{a: 1, b: 1, want: 2},
+		{a: 5, b: 0, want: 5},
+		{a: -5, b: -2, want: -7},
+	}
+
+	for _, tc := range testCases {
+		got := calculator.Add(tc.a, tc.b)
+
+		if tc.want != got {
+			t.Errorf("Add(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
 	}
 }
 
 func TestSubtract(t *testing.T) {
 	t.Parallel()
 
-	var want float64 = 2
-	got := calculator.Subtract(4, 2)
+	type testCase struct {
+		a, b float64
+		want float64
+	}
 
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+	testCases := []testCase{
+		{a: 2, b: 2, want: 0},
+		{a: 10, b: 1, want: 9},
+		{a: 5, b: -3, want: 8},
+		{a: -20, b: 0, want: -20},
+	}
+
+	for _, tc := range testCases {
+		got := calculator.Subtract(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Subtract(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
 	}
 }
 
 func TestMultiply(t *testing.T) {
 	t.Parallel()
 
-	var want float64 = 9
-	got := calculator.Multiply(3, 8)
+	type testCase struct {
+		a, b float64
+		want float64
+	}
 
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+	testCases := []testCase{
+		{a: 2, b: 2, want: 4},
+		{a: 1, b: 1, want: 1},
+		{a: 5, b: -3, want: -15},
+		{a: 20, b: 0, want: 0},
+	}
+
+	for _, tc := range testCases {
+		got := calculator.Multiply(tc.a, tc.b)
+
+		if tc.want != got {
+			t.Errorf("Multiply(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
+
 	}
 }
