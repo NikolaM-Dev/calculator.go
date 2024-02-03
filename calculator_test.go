a90be7ea87343b0f54daf8433a994fa8dcd77e73
Juan David Merchan Torres <juan1014166@gmail.com>
2024-02-03 08:11:42 -0500

    test(calculator_test): Add base tests for calculator package

---
+++ calculator_test.go
@@ -0,0 +1,18 @@
+package calculator_test
+
+import (
+	"testing"
+
+	"github.com/NikolaM-Dev/calculator"
+)
+
+func TestAdd(t *testing.T) {
+	t.Parallel()
+
+	var want float64 = 4
+	got := calculator.Add(2, 2)
+
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
+	}
+}
