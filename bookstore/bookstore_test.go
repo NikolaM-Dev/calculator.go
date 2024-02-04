c780482dd5f55bf91f1a1b84ecf41c99d43262a1
Juan David Merchan Torres <juan1014166@gmail.com>
2024-02-04 15:33:05 -0500

    test(bookstore_test): Add TestGetAllBooks test

--- bookstore/bookstore_test.go
+++ bookstore/bookstore_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/NikolaM-Dev/for-the-love-of-go/bookstore"
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestBuy(t *testing.T) {
@@ -41,5 +42,25 @@ func TestBuyErrorsIfNoCopiesLeft(t *testing.T) {
 	_, err := bookstore.Buy(b)
 	if err == nil {
 		t.Error("want error buying from zero copies, got nil")
+	}
+}
+
+func TestGetAllBooks(t *testing.T) {
+	t.Parallel()
+
+	catalog := []bookstore.Book{
+		{Title: "For the Love of Go"},
+		{Title: "The Power of Go: Tools"},
+	}
+
+	want := []bookstore.Book{
+		{Title: "For the Love of Go"},
+		{Title: "The ower of Go: Tools"},
+	}
+
+	got := bookstore.GetAllBooks(catalog)
+
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
 	}
 }
