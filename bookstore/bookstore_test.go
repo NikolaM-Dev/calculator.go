6cbd68b0488dcfda5e41a6d0ae023c39605f063b
Juan David Merchan Torres <juan1014166@gmail.com>
2024-02-04 12:04:23 -0500

    test(bookstore_test): Add test to Buy func

--- bookstore/bookstore_test.go
+++ bookstore/bookstore_test.go
@@ -6,12 +6,26 @@ import (
 	"github.com/NikolaM-Dev/for-the-love-of-go/bookstore"
 )
 
-func TestBook(t *testing.T) {
+func TestBuy(t *testing.T) {
 	t.Parallel()
 
-	_ = bookstore.Book{
+	b := bookstore.Book{
 		Title:  "Spark Joy",
 		Author: "Marie Kondo",
 		Copies: 2,
 	}
+
+	want := 1
+
+	result, err := bookstore.Buy(b)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	got := result.Copies
+
+	if got != want {
+		t.Errorf("want %d, got %d", want, got)
+	}
+}
 }
